e1c2a1bf9a7e789a1fdf67804a8e16e6694578a3
남기원 <aa8791@naver.com>
2024-11-27 17:00:06 +0900

    p150 q5~q6 문풀

---
+++ java/chap03/src/sec02/exam04/p150q5q6.java
@@ -0,0 +1,33 @@
+package sec02.exam04;
+
+public class p150q5q6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//q5.
+		double var1 = 5;
+		double var2 = 2;
+		double var3 = var1 / var2;
+		int var4 = (int) (var3 * var2);
+		System.out.println(var1);
+		System.out.println(var2);
+		System.out.println(var3);
+		System.out.println(var4);
+		
+		//q5 (다른접근법)
+		int var1_1 = 5;
+		int var2_1 = 2;
+		double var3_1 = var1_1 / var2_1;
+		int var4_1 = (int) (var3_1 * var2_1);
+		System.out.println(var1_1);
+		System.out.println(var2_1);
+		System.out.println(var3_1);
+		System.out.println(var4_1);
+		
+		//q6. Chat gpt 도움을 구함
+		int value = 356;
+		System.out.println((int)(356 / 100) * 100 );
+	}
+
+}
