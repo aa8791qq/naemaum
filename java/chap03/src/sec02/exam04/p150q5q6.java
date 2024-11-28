31b8a76ff2da23590919f70baa32271ce8f35535
남기원 <aa8791@naver.com>
2024-11-28 10:30:23 +0900

    문제풀이

--- java/chap03/src/sec02/exam04/p150q5q6.java
+++ java/chap03/src/sec02/exam04/p150q5q6.java
@@ -5,6 +5,7 @@ public class p150q5q6 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+// >>>>
 		//q5.
 		double var1 = 5;
 		double var2 = 2;
@@ -18,7 +19,7 @@ public class p150q5q6 {
 		//q5 (다른접근법)
 		int var1_1 = 5;
 		int var2_1 = 2;
-		double var3_1 = var1_1 / var2_1;
+		double var3_1 = (double)var1_1 / var2_1;
 		int var4_1 = (int) (var3_1 * var2_1);
 		System.out.println(var1_1);
 		System.out.println(var2_1);
@@ -28,6 +29,25 @@ public class p150q5q6 {
 		//q6. Chat gpt 도움을 구함
 		int value = 356;
 		System.out.println((int)(356 / 100) * 100 );
+		
+		//q6 (모범답안) ---------------------
+		int value1 = 356;
+		System.out.println((int)(value1 / 100) * 100 );
+		
+		//q6 다른접근법
+		int value2 = 356;
+		System.out.println(value2 - ( value2 % 100 ));
+		
+		//깜짝퀴즈 //실패(*1000 까지는 했음)
+		// v1 / v2
+		double v1 = 1000;
+		double v2 = 457;
+		
+		//코딩
+		double v3 = (v1 / v2) * 1000;
+		int v4 = (int)v3;
+		double v5 = (double) v4 / 1000;
+		System.out.println( v5 );
 	}
 
 }
