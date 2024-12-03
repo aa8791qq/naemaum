9b40039b52afde700b0fba54b233e72c08a12132
남기원 <aa8791@naver.com>
2024-12-03 16:07:43 +0900

    7교시 은행문제(거의 완성)

--- java/chap04/src/sec01/exam01/Bankexam.java
+++ java/chap04/src/sec01/exam01/Bankexam.java
@@ -16,31 +16,36 @@ public class Bankexam {
 			System.out.println("---------------------------------------");
 			System.out.println("선택> ");
 			int sel = scan.nextInt();
+
 			if (sel == 1) {
 				System.out.println("예금액 : ");
 				int input = scan.nextInt();
 				if (input > 0) {
 					balance += input;
+					System.out.println("입금액 : "+ in1put);
 				} else {
 					System.out.println("예금액을 확인해줄랭?");
 					System.out.println(balance);
 				}
-			} else if(sel == 2) {
+			} else if (sel == 2) {
 				System.out.println("출금액 : ");
 				int output = scan.nextInt();
 				if (output < 0) {
-					balance -= output;
+					System.out.println("출금액을 확인해주세요");
 				} else {
-					System.out.println("출금액을 확인해줄랭?");
+					System.out.println("출금액 : " + output);
+					balance -= output;
+
+					System.out.println("출금후잔고 : " + balance);
 				}
-			} else if(sel == 3) {
+			} else if (sel == 3) {
 				System.out.println("잔고 : " + balance);
-//			} else if(sel == 4)
-//	}
+			} else if (sel == 4) {
+				break;
 			}
-		}
+		} while (true);
+	}
 //		System.out.println("출금액을 확인해주세요");
-}
 
 //}
 }
