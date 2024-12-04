35e707090932f8bd56fecbf276b1026ff8b5476f
남기원 <aa8791@naver.com>
2024-12-04 12:47:22 +0900

    4교시

--- java/chap05/src/sec01/exam01/ArrayExam.java
+++ java/chap05/src/sec01/exam01/ArrayExam.java
@@ -22,8 +22,30 @@ public class ArrayExam {
 		int[] intArray = new int[3];
 		intArray[0] = 90; //변수명 = 변수값
 		intArray[1] = 85;
-		intArray[2] = 92;
+//		intArray[2] = 92;
+		
+		System.out.println(intArray[0]);
+		// 기본값은 0, false, null로 초기화된다.
+		System.out.println(intArray[2]);
+		// 범위에 벗어남(현재 : 0~2까지만 가능)
+//		System.out.println(intArray[3]);
+		
+		//선언 2번째 방법
+		//넣을 값을 몯 정확히 알고 있는 경우
+		int[] intArray2 = new int[] {90, (int)Math.random(), 70};
+		
+		int[] intArray2_1 = null;
+		intArray2_1 = new int[] {90, (int)Math.random(), 70};
+		intArray2_1 = new int[] {90, 7, (int)Math.random(), 70};
+		System.out.println(intArray2_1.length);
+		
+		//선언 3번째방법(new int[] 생략)
+		//선언과 동시에 초기화 하는 경우에만 생략가능
+		int[] intArray3 = {90, (int)Math.random(), 70};
+//		intArray3 = {90, (int)Math.random(), 70};
 
+		System.out.println(intArray3.length);
+		System.out.println(intArray3);
 	}
 
 }
