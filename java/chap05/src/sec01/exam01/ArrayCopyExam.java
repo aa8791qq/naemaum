59ace254b63dcbecc14c5b167034ebacdeca993d
남기원 <aa8791@naver.com>
2024-12-05 15:45:20 +0900

    6교시

---
+++ java/chap05/src/sec01/exam01/ArrayCopyExam.java
@@ -0,0 +1,20 @@
+package sec01.exam01;
+
+public class ArrayCopyExam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//1호점
+		String[] menu1 = new String[3];
+		menu1[0] = "망고요구르트";
+		menu1[1] = "복숭아아이스티";
+		menu2[2] = "안유진친필사인이 담긴 주스"
+		
+		//2호점(1호점과 같은메뉴판매하기)
+		/*
+		 * 
+		 */
+	}
+
+}
---
+++ java/chap05/src/sec01/exam01/VoteExam.java
@@ -0,0 +1,22 @@
+package sec01.exam01;
+
+public class VoteExam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int start = 3;
+		int end = 10;
+		
+		double drandom = Math.random();
+		int irandom = (int)drandom*(end - start + 1);
+		
+		int rerandom = irandom + start;
+		
+		//한줄로 <<<<<<< 배열에 중복된 번호???뽑기 파트 못들음
+		int random = ((int)(Math.random() * (end - start + 1))) + start;
+		
+		System.out.println(random);
+	}
+
+}
