1f8ecf189e77fe4765640db558c555530d1d3eed
남기원 <aa8791@naver.com>
2024-12-10 16:42:45 +0900

    7교시

---
+++ java/chap06/src/quiz/Song.java
@@ -0,0 +1,33 @@
+package quiz;
+
+public class Song {
+
+	//노래 만들기 테이블 양식
+	String title;	// 제목
+	String singer;	// 가수명
+	int clike;		// 좋아요수
+	
+	/**
+	 * 제목을 전달 받아서
+	 * title를 변경하는 메소드 만들기
+	 * 
+	 * 메소드명 : setTitle
+	 * 
+	 * @param
+	 * @return void
+	 * 
+	 */
+	// 메소드 선언 양식
+	// 리턴타입 메스드명 (전달인자들) { }
+	
+	
+//	void setTitle(String title) {
+//		this.title = title;
+////		return;
+//	}
+	
+	
+	
+	
+	
+}
---
+++ java/chap06/src/quiz/SongExam.java
@@ -0,0 +1,34 @@
+package quiz;
+
+public class SongExam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// 필드 복습중
+		// song 클래스를 생성
+		// 제목과 가수명을 넣고 제목, 가수명, 좋아요 수를 출력
+		Song song1 = new Song(); // 클래스명 생성 실수.
+		song1.title /* 변수 */ = "home sweet home";
+		song1.singer = "G-Dragon";
+
+		System.out.println(song1.title);
+		System.out.println(song1.singer);
+		System.out.println(song1.clike);
+
+		Song song2 = new Song();
+		song2.title = "APT";
+		song2.singer = "로제";
+
+		System.out.println(song2.title);
+		System.out.println(song2.singer);
+		System.out.println(song2.clike);
+
+		// song3은 setTitle()을 이용하기
+		Song song3 = new Song();
+		song3.setTitle("Whiplash");
+		System.out.println(song3.title);
+//		System.out.println(song3.singer);
+	}
+
+}
