d8a0cc8d67a85432f5631e2554ff65da6797b595
남기원 <aa8791@naver.com>
2024-12-10 12:50:35 +0900

    4교시 약간수정

--- java/chap06/src/sec03/exam01/KimbabExam.java
+++ java/chap06/src/sec03/exam01/KimbabExam.java
@@ -5,7 +5,7 @@ public class KimbabExam {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Kimbab cheonanStation = new Kimbab(1); // 배열 갯수
+		Kimbab cheonanStation = new Kimbab(1000); // 시드머니
 		cheonanStation.order(1); // 배열 인덱스
 		
 		cheonanStation.makeRoll("라면");
