97d3c737c7a923aa995d125163b2f1a804621aca
남기원 <aa8791@naver.com>
2024-12-10 10:57:35 +0900

    2교시

--- java/chap06/src/sec03/exam01/Car.java
+++ java/chap06/src/sec03/exam01/Car.java
@@ -4,9 +4,55 @@ public class Car {
 
 	// 생성자  ---> 약간 못들음..
 	// 기본생성자
-	public Car() { // public 생략가능
+	
+	String brand = "아반떼";
+	String color = null;
+	int cc = 0;
+//	color= "횐색"
+//	System.out.println
+	
+	// 클래스를 new로 생성할때
+	// 1. 필드가 먼저 선언된다.
+	// 2. 생성자가 실행된다.
+//	public Car() {	// public 생략가능
 					// 하나라도 생성자를 코딩하면 기본 생성자는 만들어지지 않는다.
+//	} // 오버로딩
+	
+	Car(int c) {
+		cc = c;
+		color= "횐색"; // 선언과 동시에 초기화를 1번 할 수 있다.(재할당 불가능)
+			System.out.println("Car(int) 생성");
+		test();
+	}
+	
+	Car(String b){
+		brand = b;
+		System.out.println(brand);				
+	}
+	
+	Car(){
+		this("아빈떼", "흰색", 1300);
+		System.out.println("아무것도 없지롱~");
+		}
+	
+	Car(String b, String c, int cc2){
+//		다른 생성자 호출하는 방법
+//		Car() 호출
+//		무조건 첫번째 줄에서 호출해야 한다.
+//		this();
 		
+		brand = b;
+		color = c ;
+		cc = cc2;
+		System.out.println(brand + "," + color + "," + cc);
+		
+//		Car() 호출
+//		this();
+	}
+	
+	void test() {
+		System.out.println("test() 실행");
+//		Car(); 메소드
 	}
 	
 }
---
+++ java/chap06/src/sec03/exam01/CarExam.java
@@ -0,0 +1,15 @@
+package sec03.exam01;
+
+public class CarExam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Car c1 = new Car(1300); //새로 생성하자마자 실행할 수 있는 것!
+		Car c2 = new Car("소나타 dn8"); //새로 생성하자마자 실행할 수 있는 것!
+		Car c3 = new Car();
+//		c3.brand = "스파크";
+		
+	}
+
+}
