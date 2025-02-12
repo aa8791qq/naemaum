486e0132b4315956e260204705d15f9b8544dce8
남기원 <aa8791@naver.com>
2025-02-12 16:17:18 +0900

    getsetter

--- java/chap06/src/sec06/exam06/getset/Car2.java
+++ java/chap06/src/sec06/exam06/getset/Car2.java
@@ -8,12 +8,15 @@ public class Car2 {
 	public int getSpeed() {
 		return speed;
 	}
+	
 	public void setSpeed(int speed) {
 		this.speed = speed;
 	}
+	
 	public boolean isStop() {
 		return stop;
 	}
+	
 	public void setStop(boolean stop) {
 		this.stop = stop;
 	}
