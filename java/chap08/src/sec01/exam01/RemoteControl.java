62e392a1ba56f99eb056eccdf6c403e3e4c84ce2
남기원 <aa8791@naver.com>
2025-02-19 13:24:43 +0900

    인터페이스

---
+++ java/chap08/src/sec01/exam01/RemoteControl.java
@@ -0,0 +1,20 @@
+package sec01.exam01;
+
+public interface RemoteControl {
+
+	/* [public static final] 생략가능 */
+	int MAX_VOLUME = 10;
+	int MIN_VOLUME = 0;
+	
+	/* [public abstract] 생략가능 */
+	void turnOn();
+	void turnOff();
+	void setVolume(int vol);
+	
+	// 이해못함(default 부분)
+	default void mic(String text) {
+		
+	}
+	
+	
+}
