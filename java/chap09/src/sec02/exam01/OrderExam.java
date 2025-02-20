140ecca2d969557b2718be445806b37f1e00b030
남기원 <aa8791@naver.com>
2025-02-20 15:48:56 +0900

    익명함수

--- java/chap09/src/sec02/exam01/OrderExam.java
+++ java/chap09/src/sec02/exam01/OrderExam.java
@@ -16,10 +16,10 @@ public class OrderExam {
 
 		m1.name = "다솜";
 		m1.age = 27;
-		
+
 		m2.name = "서영";
 		m2.age = 25;
-		
+
 		m3.name = "윤희";
 		m3.age = 24;
 
@@ -27,17 +27,44 @@ public class OrderExam {
 		list.add(m1);
 		list.add(m2);
 		list.add(m3);
-		
-//		if(m1>m3) {} / 클래스임
-		
-		Comparator c = new OrderBy();	// 부모가 있어서 가능한 코딩
-		Collections.sort(list, c);
 
-		for(int i = 0; i<list.size(); i++) {
-			System.out.println(((MemberDTO)list.get(i)).age); // 형변환
-			System.out.println(((MemberDTO)list.get(i)).name); // 형변환
+//		if(m1>m3) {} / 클래스임
+
+		Comparator c = new OrderBy(); // 부모가 있어서 가능한 코딩 // 파괴함수
+
+		// 익명함수화
+		Comparator c2 = new Comparator() {
+			public int a = 10; // 내부에서는 시용가능
+			public void test() {
+				// 이름이 없어서 밖에서 못씀
+			}
+			
+			@Override
+			public int compare(Object o1, Object o2) {
+				MemberDTO dto1 = (MemberDTO) o1;
+				MemberDTO dto2 = (MemberDTO) o2;
+
+				int result = dto1.age - dto2.age;
+
+				return result;
+			}
+
+		}; // 부모가 있어서 가능한 코딩 // 파괴함수
+
+		Collections.sort(list, c); // 필수
+
+		for (int i = 0; i < list.size(); i++) {
+			System.out.println(((MemberDTO) list.get(i)).age); // 형변환
+			System.out.println(((MemberDTO) list.get(i)).name); // 형변환 // 추가작업(String 연산 불가)
 		}
 		
+		Collections.sort(list, c2); // 필수
+		
+		for (int i = 0; i < list.size(); i++) {
+			System.out.println(((MemberDTO) list.get(i)).age); // 형변환
+			System.out.println(((MemberDTO) list.get(i)).name); // 형변환 // 추가작업(String 연산 불가)
+		}
+
 	}
 
 }
