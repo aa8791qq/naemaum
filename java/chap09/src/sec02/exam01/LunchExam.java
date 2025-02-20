c5eac561a8ca7c35caeb463c8768c7705861a50f
남기원 <aa8791@naver.com>
2025-02-20 11:51:17 +0900

    익명함수

---
+++ java/chap09/src/sec02/exam01/LunchExam.java
@@ -0,0 +1,13 @@
+package sec02.exam01;
+
+public class LunchExam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Lunch lunch = new Lunchimpl();
+		lunch.order(null);
+		
+	}
+
+}
---
+++ java/chap09/src/sec02/exam01/Lunchimpl.java
@@ -0,0 +1,13 @@
+package sec02.exam01;
+
+public class Lunchimpl implements Lunch {
+
+	@Override
+	public int order(String menu) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("오더 실행");
+		return 0;
+	}
+
+}
