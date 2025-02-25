22bb014d1d6da81c16f72b0b631871e40b44b966
남기원 <aa8791@naver.com>
2025-02-25 15:03:15 +0900

    스프링

--- java/proj1_hello/src/main/java/HelloServlet.java
+++ java/proj1_hello/src/main/java/HelloServlet.java
@@ -13,6 +13,7 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet implementation class HelloServlet
  */
 //@WebServlet("/HelloServlet")
+//@WebServlet("/hello.html")
 @WebServlet("/hello")
 public class HelloServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
