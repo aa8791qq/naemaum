de8270aa1498279d917735b3726fdaa6d4989396
남기원 <aa8791@naver.com>
2025-02-25 12:47:48 +0900

    스프링

--- java/proj1_hello/src/main/java/HelloServlet.java
+++ java/proj1_hello/src/main/java/HelloServlet.java
@@ -1,6 +1,8 @@
 
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,7 +12,8 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class HelloServlet
  */
-@WebServlet("/HelloServlet")
+//@WebServlet("/HelloServlet")
+@WebServlet("/hello")
 public class HelloServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -26,11 +29,21 @@ public class HelloServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
+    // 놓침.. 닉..
     @Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("<strong>Served</strong> at: ").append(request.getContextPath());
-	}
+//		response.getWriter().append("<strong>Served</strong> at: ").append(request.getContextPath());
+	
+		PrintWriter out = response.getWriter();
+		out.println("<h1>");
+		out.print("hello?");
+		out.println("</h1>");
+		
+		for(int i = 0; i < 5; i++) {
+			out.println(i + "<br>");
+		}
+    }
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
