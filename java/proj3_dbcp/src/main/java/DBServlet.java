9360a9aef4e2376f1a7b70823908f1f9088af121
남기원 <aa8791@naver.com>
2025-02-26 16:52:51 +0900

    JDBC

--- java/proj3_dbcp/src/main/java/DBServlet.java
+++ java/proj3_dbcp/src/main/java/DBServlet.java
@@ -5,6 +5,10 @@ import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
@@ -14,6 +18,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
+
+import kr.or.human.DTO.EmpDTO;
 
 @WebServlet("/DB")
 public class DBServlet extends HttpServlet {
@@ -30,7 +36,7 @@ public class DBServlet extends HttpServlet {
 			DataSource ds = (DataSource) ctx.lookup("java:/comp/env/jdbc/oracle");
 			// 커넥션 풀에서 접속 정보를 가져오기
 			// 접속이 안되면 null(id, pw 불일치 상황)
-			Connection con = ds.getConnection();
+			Connection con = ds.getConnection(); // null 방어코딩해야됨
 			
 			// SQL 준비
 			String query = "select * from emp";	// 여러줄로 쓸때에는 공백삽입을 주의해야함(변경가능)
@@ -45,6 +51,8 @@ public class DBServlet extends HttpServlet {
 			// 결과활용
 //			rs.next(); 	// 다음줄로 커서를 이동(1번째 줄)
 						// 다음줄이 있으면 true, 없으면 false를 return
+//			List<Map> list = new ArrayList<Map>();
+			List<EmpDTO> list = new ArrayList<EmpDTO>();
 			while(rs.next()) { // 다음줄이 없을때 까지 반복
 				int empno = rs.getInt("empno");
 				String ename = rs.getString("ename");
@@ -54,7 +62,26 @@ public class DBServlet extends HttpServlet {
 				System.out.println(empno);
 				System.out.println(ename);
 				System.out.println(hiredate);
+				
+				
+				Map map = new HashMap();
+				map.put("empno", empno);
+				map.put("ename", ename);
+				map.put("hiredate", hiredate);
+//				list.add(map);
+				
+				//실무
+				EmpDTO empDTO = new EmpDTO();
+				empDTO.setEmpno(empno);
+				empDTO.setEname(ename);
+				empDTO.setHiredate(hiredate);
+				list.add(empDTO);
+				
 			}
+			
+			EmpDTO map = list.get(0);
+			
+			
 			
 		} catch(Exception e) {
 			e.printStackTrace();
