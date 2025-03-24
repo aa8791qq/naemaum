97900971279698b4fef0ef7b96497ce1e0dbc30c
남기원 <aa8791@naver.com>
2025-03-24 12:50:11 +0900

    spring

--- java/spring1_hello/src/main/java/kr/or/human/controller/MemberController.java
+++ java/spring1_hello/src/main/java/kr/or/human/controller/MemberController.java
@@ -1,5 +1,19 @@
 package kr.or.human.controller;
 
-public class MemberController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+public class MemberController {
+	
+	MemberController(){
+		System.out.println("생성자");
+	}
+
+	@RequestMapping("/member")
+	void listMember() {
+		System.out.println("list 맴버 실행");
+	}
+	
+	
 }
