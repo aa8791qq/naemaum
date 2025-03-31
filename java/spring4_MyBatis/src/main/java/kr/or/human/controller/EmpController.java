433a653f63b9f0218df19887e631b93863679acf
남기원 <aa8791@naver.com>
2025-03-31 16:09:58 +0900

    311spring

--- java/spring4_MyBatis/src/main/java/kr/or/human/controller/EmpController.java
+++ java/spring4_MyBatis/src/main/java/kr/or/human/controller/EmpController.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.Model;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -33,7 +36,7 @@ public class EmpController {
 //		return "emp";
 //	}
 	
-	@RequestMapping(value ="/emp1", method = RequestMethod.GET) 
+	@RequestMapping(value ="/emp2", method = RequestMethod.GET) 
 	public String EmpAsk() {
 		System.out.println("들어왔어");
 		EmpDTO c = empService.getEmpnoAsk();
@@ -41,4 +44,10 @@ public class EmpController {
 		return "emp";
 	}
 	
+	@RequestMapping(value ="/emp5", method = RequestMethod.PUT) 
+	public String modifyEmp(Model model) {
+		System.out.println("들어왔어");
+		// 실제 update 장소
+		
+	}
 }
